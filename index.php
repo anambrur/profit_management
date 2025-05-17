315794343d288eb6b93a3c2cfb4a5c60980b4292
anambrur <anambrur@gmail.com.com>
2025-05-17 18:34:20 +0600

    resolve conflict

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 "hello world";
 
 "my name is siddharth";
-"hello world"
-"hello world2"
-"hello world3"
-"hello world4"
-"hello world5"
+"hello world";
+"hello world2";
+"hello world3";
+"hello world4";
+"hello world5";
 ?>
