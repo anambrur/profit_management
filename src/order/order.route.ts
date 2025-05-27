ca9a8b26ccdf877e589bba691ce069ccde98a9b7
mashruf-ahmed55 <mashrufahmed03@gmail.com>
2025-05-27 18:48:44 +0600

    more problem slove

--- src/order/order.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getAllOrders } from './order.controller.js';
-
-
-const orderRouter = Router();
-
-orderRouter.route('/get-all-orders/:id').get(getAllOrders);
-
-export default orderRouter;
