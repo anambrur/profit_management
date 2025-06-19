2a5919e17279698e15f007e60d2e5e50d60356a2
mashruf-ahmed55 <mashrufahmed03@gmail.com>
2025-06-19 10:51:37 +0600

    create product history

--- src/productHistory/productHistory.controller.ts
+++ src/productHistory/productHistory.controller.ts
@@ -1,6 +1,44 @@
 import type { NextFunction, Request, Response } from 'express';
 import createHttpError from 'http-errors';
 import productHistoryModel from './productHistory.model';
+import Product from '../product/product.model';
+
+export const createProductHistory = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const id = req.params.id;
+    const product = await Product.findById(id);
+    if (!product) {
+      return next(createHttpError(404, 'Product not found'));
+    }
+    const newProduct = await productHistoryModel.create({
+      productId: product._id,
+      ...req.body,
+    });
+    res.status(201).json({ newProduct, success: true });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getSingleProduct = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const product = await productHistoryModel.findById(req.params.id);
+    if (!product) {
+      return next(createHttpError(404, 'Product not found'));
+    }
+    res.status(200).json({ product, success: true });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const updateProduct = async (
   req: Request,
