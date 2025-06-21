54804870046954c7dc02e8cbb9e584c715d7b1b2
mashruf-ahmed55 <mashrufahmed03@gmail.com>
2025-06-21 19:09:28 +0600

    create order all api data

--- src/order/order.route.ts
+++ src/order/order.route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { getAllOrders } from './order.controller';
+import { getAllOrders, getOrders } from './order.controller';
 
 const orderRouter = Router();
 
 orderRouter.route('/get-all-orders').get(getAllOrders);
+orderRouter.route('/get-orders').get(getOrders);
 
 export default orderRouter;
