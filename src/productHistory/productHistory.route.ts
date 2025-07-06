cbadb17b1fbf84233e15f1e293f1278cbf450528
mashruf-ahmed55 <mashrufahmed03@gmail.com>
2025-07-06 11:21:19 +0600

    delete Profit route

--- src/productHistory/productHistory.route.ts
+++ src/productHistory/productHistory.route.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
+import { csvUpload } from '../middlewares/multer.js';
 import {
   bulkUploadProductHistory,
   createProductHistory,
+  deleteProduct,
   getAllProductHistory,
   getProductHistoryList,
   updateSingleField,
 } from './productHistory.controller.js';
-import { csvUpload } from '../middlewares/multer.js';
 
 const productHistoryRouter = Router();
 
@@ -28,13 +29,17 @@ productHistoryRouter
   .route('/get-product-history-list/:id')
   .get(getProductHistoryList);
 
+productHistoryRouter
+  .route('/delete-product-history/:id')
+  .delete((req, res, next) => {
+    Promise.resolve(deleteProduct(req, res, next)).catch(next);
+  });
 
-
-  productHistoryRouter.post(
-    '/upload-product-history',
-    csvUpload.single('file'),
-    (req, res, next) => {
-      Promise.resolve(bulkUploadProductHistory(req, res, next)).catch(next);
-    }
-  );
+productHistoryRouter.post(
+  '/upload-product-history',
+  csvUpload.single('file'),
+  (req, res, next) => {
+    Promise.resolve(bulkUploadProductHistory(req, res, next)).catch(next);
+  }
+);
 export default productHistoryRouter;
